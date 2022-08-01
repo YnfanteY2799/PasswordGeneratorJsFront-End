ab41a435d2dafabb134eef6a657b767d56e19588
Yassett J Ynfante Jimenez <49245097+YnfanteY2799@users.noreply.github.com>
2022-08-01 14:07:40 -0400

    + finished simple react router conf

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,11 @@
 import { ReactElement } from "react";
+import { BeakerIcon } from "@heroicons/react/solid";
 
 export default function Home(): ReactElement {
-  return <>HomePage</>;
+  return (
+    <>
+      <BeakerIcon className="h-25 w-25 text-primary" />
+      HomePage
+    </>
+  );
 }
--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,7 +1,19 @@
 import { ReactElement } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home/Home";
 
-
+const routes: Array<{ name: string; Component: Function }> = [
+  { name: "/", Component: Home },
+];
 
 export default function Index(): ReactElement {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        {routes.map(({ name, Component }) => (
+          <Route path={name} element={<Component />} />
+        ))}
+      </Routes>
+    </BrowserRouter>
+  );
 }
